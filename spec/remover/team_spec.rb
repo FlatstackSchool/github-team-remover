76368df818d41320f1d738649a7d5c964951675a
Nickolay Abdrafikov <nickolay.abdrafikov@flatstack.com>
2014-03-28 12:28:32 +0400

    Implement team_spec

--- spec/remover/team_spec.rb
+++ spec/remover/team_spec.rb
@@ -4,10 +4,22 @@ describe Remover::Team do
 
   let(:team) { Remover::Team.new(github_client, github_team)}
 
-  context 'without members' do
-    before do
-      allow(github_client).to receive(:team_members) { [] }
+  let(:members) { [double('Github Member')] }
+  let(:repositories) { [double('Github Repository')] }
+
+  before do
+    allow(github_client).to receive(:team_members) { members }
+    allow(github_client).to receive(:team_repositories) { repositories }
+  end
+
+  context 'with full of members and repositories' do
+    it 'not used' do
+      expect(team).to be_used
     end
+  end
+
+  context 'without members' do
+    let(:members) { [] }
 
     it 'used' do
       expect(team).not_to be_used
@@ -15,20 +27,19 @@ describe Remover::Team do
   end
 
   context 'without repositories' do
+    let(:repositories) { [] }
+
     it 'used' do
       expect(team).not_to be_used
     end
   end
 
   context 'without members and repositories' do
+    let(:members) { [] }
+    let(:repositories) { [] }
+
     it 'used' do
       expect(team).not_to be_used
-    end
-  end
-
-  context 'with full of members and repositories' do
-    it 'not used' do
-      expect(team).to be_used
     end
   end
 end
